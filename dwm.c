9f723d8422552b4fb233d4ac96aeba127fb36a10
Edvard Thörnros <edvard.thornros@gmail.com>
2021-06-27 21:28:34 +0200

    A better implementation of gaps

--- dwm.c
+++ dwm.c
@@ -1116,7 +1116,7 @@ monocle(Monitor *m)
 	if (n > 0) /* override layout symbol */
 		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
-		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+		resize(c, m->wx + gappx, m->wy + gappx, m->ww - 2 * c->bw - 2 * gappx , m->wh - 2 * c->bw - 2 * gappx, 0);
 }
 
 void
@@ -1693,7 +1693,7 @@ tagmon(const Arg *arg)
 void
 tile(Monitor *m)
 {
-	unsigned int i, n, h, mw, my, ty;
+	unsigned int i, n, h, r, mw, my, ty;
 	Client *c;
 
 	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
@@ -1701,20 +1701,32 @@ tile(Monitor *m)
 		return;
 
 	if (n > m->nmaster)
-		mw = m->nmaster ? m->ww * m->mfact : 0;
+		mw = m->nmaster ? (m->ww - gappx) * m->mfact : 0;
 	else
 		mw = m->ww;
 	for (i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
 		if (i < m->nmaster) {
-			h = (m->wh - my) / (MIN(n, m->nmaster) - i);
-			resize(c, m->wx, m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0);
+            r = MIN(n, m->nmaster) - i;
+            h = (m->wh - my - gappx * r) / r;
+			resize(c,
+                m->wx + gappx,
+                m->wy + my + gappx,
+                mw - (2*c->bw) - 2 * gappx,
+                h - (2*c->bw) - gappx, 0);
 			if (my + HEIGHT(c) < m->wh)
-				my += HEIGHT(c);
+				my += HEIGHT(c) + gappx;
 		} else {
-			h = (m->wh - ty) / (n - i);
-			resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), 0);
+            r = n - i;
+ 			h = (m->wh - ty - gappx * r) / r;
+ 			resize(
+                c,
+                m->wx + mw,
+                m->wy + ty + gappx,
+                m->ww - mw - gappx - (2*c->bw),
+                h - (2*c->bw) - gappx,
+                False);
 			if (ty + HEIGHT(c) < m->wh)
-				ty += HEIGHT(c);
+				ty += HEIGHT(c) + gappx;
 		}
 }
 
