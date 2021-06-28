f04c796c86f9a8b034f1c26ad53bb597d6560077
Edvard Thörnros <edvard.thornros@gmail.com>
2021-06-28 22:39:59 +0200

    Update font

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 5;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monaco:size=11" };
+static const char *fonts[]          = { "LiberationMono:size=11" };
 static const char col_gray[]        = "#18150d";
 static const char col_fg[]          = "#978965";
 static const char col_hi[]          = "#c0b18b";
